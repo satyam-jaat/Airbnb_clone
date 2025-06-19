5daa4e857af517af88123841400d31fb6f30cefa
Satyam Tomar <satyamtomar41015@gmail.com>
2025-06-19 21:18:00 +0530

    updated

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const Listing = require("./models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+  });
+
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("successful testing");
+});
+
+app.listen(8080, () => {
+    console.log("Server is listening at port 8080");
+})
